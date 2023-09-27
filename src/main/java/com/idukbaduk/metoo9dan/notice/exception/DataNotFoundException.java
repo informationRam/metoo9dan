df11b106ca81d097d2161894264ade65d99a9c91
informationRam <qhfka0527@gmail.com>
2023-09-27 21:48:14 +0900

    230927 21:48 공지 상세조회 초안 완성. 공지 목록 수정

---
+++ src/main/java/com/idukbaduk/metoo9dan/notice/exception/DataNotFoundException.java
@@ -0,0 +1,12 @@
+package com.idukbaduk.metoo9dan.notice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+//조회된 결과가 없을 경우 발생하는 예외 처리 클래스
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "ENTITY NOT FOUND")
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String msg) {
+        super(msg);
+    }
+}
