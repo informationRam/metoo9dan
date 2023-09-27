066dbb89472daeb81ceb2635ed70015cfe933eda
ryuahneee <ryuahneee@gmail.com>
2023-09-27 17:44:39 +0900

    230927 17:41 - member, EducatorInfo 1:1 관계 entity 테스트 완료.

--- src/main/java/com/idukbaduk/metoo9dan/common/entity/EducatorInfo.java
+++ src/main/java/com/idukbaduk/metoo9dan/common/entity/EducatorInfo.java
@@ -15,7 +15,6 @@ public class EducatorInfo {
     private Integer educator_info_no;
 
     @OneToOne(fetch = FetchType.LAZY)
-    //@MapsId //@MapsId 는 @id로 지정한 컬럼에 @OneToOne 이나 @ManyToOne 관계를 매핑시키는 역할
     @JoinColumn(name = "member_no", referencedColumnName = "member_no") // 외래 키 설정
     private Member member;
 
