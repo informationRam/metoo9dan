1d3cbfc10842cc960613a954b8d7960acfeb1fee
ryuahneee <ryuahneee@gmail.com>
2023-09-27 11:35:23 +0900

    230927 11:35 - 민희님 코드 머지함

--- src/main/java/com/idukbaduk/metoo9dan/common/entity/EducatorInfo.java
+++ src/main/java/com/idukbaduk/metoo9dan/common/entity/EducatorInfo.java
@@ -13,7 +13,7 @@ public class EducatorInfo {
     @Column(name = "member_no")
     private Integer memberNo;
 
-    @OneToOne(fetch = FetchType.EAGER)
+    @OneToOne(fetch = FetchType.LAZY)
     @MapsId //@MapsId 는 @id로 지정한 컬럼에 @OneToOne 이나 @ManyToOne 관계를 매핑시키는 역할
     @JoinColumn(name = "member_no", referencedColumnName = "member_no") // 외래 키 설정
     private Member member;
