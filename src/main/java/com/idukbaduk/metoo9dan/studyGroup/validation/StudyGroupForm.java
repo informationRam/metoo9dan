af2b60728f96b7d756803657928323097402dca3
min <happysss0505@gmail.com>
2023-10-02 22:17:09 +0900

    231002 22:15 학습그룹 상세등록 수정(game_content_no파라미터 받기)

--- src/main/java/com/idukbaduk/metoo9dan/studyGroup/validation/StudyGroupForm.java
+++ src/main/java/com/idukbaduk/metoo9dan/studyGroup/validation/StudyGroupForm.java
@@ -2,36 +2,38 @@ package com.idukbaduk.metoo9dan.studyGroup.validation;
 
 import com.idukbaduk.metoo9dan.common.entity.GameContents;
 import com.idukbaduk.metoo9dan.common.entity.Member;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@Validated
 public class StudyGroupForm {
 
     @NotEmpty(message = "그룹명은 필수 입력 입니다.")
     private String groupName; //그룹명
 
-    //@NotEmpty(message = "그룹인원은 필수 입력 입니다.")
-    @NotNull
+    @NotNull(message = "그룹인원은 필수 입력 입니다.")
     private Integer groupSize; //그룹인원
 
-    //@NotEmpty(message = "그룹 시작일은 필수 입력 입니다.")
-    @NotNull
+    @NotNull(message = "그룹 시작일은 필수 입력 입니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date groupStartDate; //그룹 시작일
 
-    //@NotEmpty(message = "그룹 종료일은 필수 입력 입니다.")
-    @NotNull
+    @NotNull(message = "그룹 종료일은 필수 입력 입니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date groupFinishDate; //그룹 종료일
 
+    @Valid
     private String groupIntroduce; //그룹 소개
 
     @NotNull
