b790f6a58002d26014b7ea9ea709b876e1c35fd5
min <happysss0505@gmail.com>
2023-10-03 16:22:51 +0900

    231002 16:22 학습그룹 수정

--- src/main/java/com/idukbaduk/metoo9dan/studyGroup/service/StudyGroupService.java
+++ src/main/java/com/idukbaduk/metoo9dan/studyGroup/service/StudyGroupService.java
@@ -30,18 +30,22 @@ public class StudyGroupService {
     private final GameContentRepository gameContentRepository;
     private final MemberRepository memberRepository;
 
+    //학습그룹 목록 조회
     public List<StudyGroupsListDTO> getList(int member_no) {
         return studyGroupRepository.getGroupList(member_no);
     }
 
+    //학습그룹 상세 조회
     public List<GroupsDetailListDTO> getDetailList(int group_no) {
         return studyGroupRepository.getGroupDetailList(group_no);
     }
 
+    //학습그룹인원 정보 가져오기
     public List<GroupsDetailListDTO> getGroupInfo(int group_no) {
         return studyGroupRepository.getGroupInfo(group_no);
     }
 
+    //학습그룹 등록
     public void add(String group_name, Integer group_size, Date group_start_date, Date group_finish_date, String group_introduce,  GameContents gameContents, Member member) {
         StudyGroups studyGroups = new StudyGroups();
         studyGroups.setGroupName(group_name);
@@ -56,17 +60,29 @@ public class StudyGroupService {
         groupMakeRepository.save(studyGroups);
     }
 
+    //게임콘텐츠 리스트
     public List<GameContentsListDTO> getGameList(int member_no) {
         return studyGroupRepository.getGameList(member_no);
     }
 
+    //학습그룹 삭제
     public void delete(StudyGroups studyGroups) {
         groupMakeRepository.delete(studyGroups);
     }
 
+    //학습그룹 가져오기
     public StudyGroups getGruop(int group_no) {
         Optional<StudyGroups> studyGroups = groupMakeRepository.findById(group_no);
         return studyGroups.get();
     }
 
+    public void modify(StudyGroups studyGroups,String group_name, Integer group_size, Date group_start_date, Date group_finish_date, String group_introduce, Member member) {
+        studyGroups.setGroupName(group_name);
+        studyGroups.setGroupSize(group_size);
+        studyGroups.setGroupStartDate(group_start_date);
+        studyGroups.setGroupFinishDate(group_finish_date);
+        studyGroups.setGroupIntroduce(group_introduce);
+        studyGroups.setMember(member);
+        groupMakeRepository.save(studyGroups);
+    }
 }
