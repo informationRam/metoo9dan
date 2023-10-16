5318f423f87a175bcb4e29640211474437b0858b
Yujin Ahn <ttangppy@gmail.com>
2023-10-16 18:32:19 +0900

    10/16 18:31 회원관리 게시판 생성

--- src/main/java/com/idukbaduk/metoo9dan/admin/controller/adminController.java
+++ src/main/java/com/idukbaduk/metoo9dan/admin/controller/adminController.java
@@ -1,17 +1,20 @@
-package com.idukbaduk.metoo9dan.member.controller;
+package com.idukbaduk.metoo9dan.admin.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RequestMapping("/admin")
 @Controller
 public class adminController {
 
     //회원관리페이지 보여주기
-    @GetMapping(value="/memberManage")
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping(value="/listMember")
     public String memberManange() throws Exception {
 
-        return "/member/memberManage";
+        return "/member/memberList";
     }
 }
