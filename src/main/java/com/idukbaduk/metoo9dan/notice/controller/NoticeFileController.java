d7ff1ad6b5329409187b5190e4ab5d708945f667
informationRam <qhfka0527@gmail.com>
2023-10-17 18:00:21 +0900

    231017 18:00 공지사항 첨부파일 수정 완료. (근데 물리파일도 삭제할 수 있어야 함) 그리고 detail이나 modifyForm 등등... principal 처리 해야함

--- src/main/java/com/idukbaduk/metoo9dan/notice/controller/NoticeFileController.java
+++ src/main/java/com/idukbaduk/metoo9dan/notice/controller/NoticeFileController.java
@@ -1,6 +1,9 @@
 package com.idukbaduk.metoo9dan.notice.controller;
 
+import com.idukbaduk.metoo9dan.common.entity.NoticeFiles;
+import com.idukbaduk.metoo9dan.notice.service.NoticeFilesService;
 import groovy.util.logging.Log4j2;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
@@ -10,19 +13,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 @Log4j2
+@RequiredArgsConstructor
 @RequestMapping("/notice")
 @Controller
 public class NoticeFileController {
     //looger
     Logger logger = LoggerFactory.getLogger(NoticeController.class);
+    private final NoticeFilesService filesService;
 
     // 파일 다운로드
     @GetMapping(value="/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
@@ -47,5 +50,28 @@ public class NoticeFileController {
             e.printStackTrace();
         }
         return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/deleteFile/{fileNo}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<Resource> deleteFile(@PathVariable("fileNo")Integer fileNo){
+        //fileName = 경로/uuid_originName
+        logger.info("deleteFile()진입, 삭제하려는 fileNo: "+fileNo);
+
+        //해당 파일 조회
+        NoticeFiles noticeFiles = filesService.selectFile(fileNo);
+        if(noticeFiles!=null){
+            //해당 파일 삭제
+            //1) 디비삭제
+            filesService.delete(noticeFiles);
+
+            //2) 물리파일삭제~~~~~~~~~~~~~~~
+        }
+
+
+        //에러난 경우 에러응답 보내기
+
+        //정상 처리된 경우 응답 보내기
+        return new ResponseEntity<Resource>(HttpStatus.OK);
+
     }
 }
