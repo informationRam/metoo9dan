c687e88d4c0e6156ab74a1b069f288e11de28902
Cha22un <2chaeun522@gmail.com>
2023-10-20 09:56:28 +0900

    231020 8:57am 평가하기 페이지 css 제외 완

---
+++ src/main/java/com/idukbaduk/metoo9dan/homework/domain/EvaluationDTO.java
@@ -0,0 +1,18 @@
+package com.idukbaduk.metoo9dan.homework.domain;
+
+import com.idukbaduk.metoo9dan.common.entity.HomeworkSend;
+import com.idukbaduk.metoo9dan.common.entity.HomeworkSubmit;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@Data
+@RequiredArgsConstructor
+@ToString
+public class EvaluationDTO {
+    private String homeworkSubmitNo;
+    private String evaluationValue;
+}
