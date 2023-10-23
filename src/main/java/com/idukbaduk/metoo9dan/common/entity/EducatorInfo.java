6dd4c1bd93d05c447c98150f45241632bf788689
informationRam <qhfka0527@gmail.com>
2023-10-23 13:59:53 +0900

    231022 13:59 양방향매핑 수정

--- src/main/java/com/idukbaduk/metoo9dan/common/entity/EducatorInfo.java
+++ src/main/java/com/idukbaduk/metoo9dan/common/entity/EducatorInfo.java
@@ -2,6 +2,7 @@ package com.idukbaduk.metoo9dan.common.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 
 //교육자 정보 테이블 -PRIMARY KEY (member_no)
 @Entity
@@ -15,9 +16,10 @@ public class EducatorInfo {
     @Column(name = "educator_info_no")
     private Integer educatorInfoNo;
 
-   // @MapsId //@MapsId 는 @id로 지정한 컬럼에 @OneToOne 이나 @ManyToOne 관계를 매핑시키는 역할 :해당 엔티티의 PK가 다른 엔티티의 PK와 동일하게 설정
+    // @MapsId //@MapsId 는 @id로 지정한 컬럼에 @OneToOne 이나 @ManyToOne 관계를 매핑시키는 역할 :해당 엔티티의 PK가 다른 엔티티의 PK와 동일하게 설정
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_no", referencedColumnName = "member_no",unique = true) // 외래 키 설정
+    @JoinColumn(name = "member_no", referencedColumnName = "member_no",unique = true)
+    @ToString.Exclude
     private Member member;
 
     @Column
